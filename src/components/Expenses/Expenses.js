9f5150d6d009b8abe23fe780d5eb075b93ad8077
Barış Gündüz <baris@gunduzmedya.com>
2023-04-12 15:32:25 +0300

    Feature - Rendering lists conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 
 import ExpensesList from "./ExpensesList";
-
+import ExpensesChart from "./ExpensesChart";
 import Card from "../UI/Card";
-import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+
+import "./Expenses.css";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState("2020");
@@ -24,6 +25,7 @@ const Expenses = (props) => {
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler}
                 />
+                <ExpensesChart expenses={filteredExpenses} />
                 <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
